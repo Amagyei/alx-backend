21ff4c51b12a62b4000187b03eb0b2b437b756cc
amagyei <kwame.na@hotmail.com>
2024-10-30 00:22:45 +0000

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,9 @@
 """
 
 def index_range(page, page_size):
+    '''
+    function that checks and returns the start page and end page for a given page and page_size 
+    '''
     start_index = page_size * (page - 1)
     end_index = start_index + page_size
     return(start_index, end_index)
