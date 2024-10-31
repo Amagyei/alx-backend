16b7cc230f33ba6249d64cf0e49072675ca8daeb
amagyei <kwame.na@hotmail.com>
2024-10-31 03:51:49 +0000

    task 2

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,7 +6,10 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
+    """
     def put(self, key, item):
         """Adds an item in the cache.
         """
@@ -14,8 +17,8 @@ class FIFOCache(BaseCaching):
             return
         self.cache_data[key] = item
         if len(cache_data) > self.MAX_ITEMS:
-            print(f"DISCARD: {next(iter(self.cache_data))}")
-            del self.cache_data[0]
+            first_key, _ - self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
         """Retrieves an item by key.
