ea75eb6fc546c0b5d46694bb0fa2b136ff33ce5b
amagyei <kwame.na@hotmail.com>
2024-10-31 04:02:35 +0000

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,7 +8,6 @@ from collections import OrderedDict
 
 class FIFOCache(BaseCaching):
     """Represents an object that allows storing and
-    
     retrieving items from a dictionary with a FIFO
     removal mechanism when the limit is reached.
     """
