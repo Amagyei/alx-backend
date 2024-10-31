8a2309c2f2d88eb4fa81970aea9f6879e584826a
amagyei <kwame.na@hotmail.com>
2024-10-31 03:37:26 +0000

    taskl

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,6 +4,7 @@
 
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
 
     def put(self, key, item):
@@ -13,12 +14,10 @@ class FIFOCache(BaseCaching):
             return
         self.cache_data[key] = item
         if len(cache_data) > self.MAX_ITEMS:
-            print(f"DISCARD: {next(iter(self.cache_dat:wq
-            a))}")
+            print(f"DISCARD: {next(iter(self.cache_data))}")
             del self.cache_data[0]
 
-     def get(self, key):
+    def get(self, key):
         """Retrieves an item by key.
         """
         return self.cache_data.get(key, None)
-
