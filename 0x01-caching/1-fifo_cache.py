a1215487f4620706cfc67acef2fe25b2de9c8b57
amagyei <kwame.na@hotmail.com>
2024-10-31 03:27:21 +0000

    task 0

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Basic FIFO caching module.
+"""
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(cache_data) > self.MAX_ITEMS:
+            print(f"DISCARD: {next(iter(self.cache_dat:wq
+            a))}")
+            del self.cache_data[0]
+
+     def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
