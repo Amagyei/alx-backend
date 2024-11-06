c402bfc0b47d1403fbb60cc14a92581f34345a88
amagyei <kwame.na@hotmail.com>
2024-11-06 00:51:43 +0000

    task 2

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+''' Basic flask app that creates a single / route
+to an index.html page that returns hello world
+'''
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+from flask_babel import Babel
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def HELLO_WORLD():
+    ''' function that returns the renderend html template
+    '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    ''' function that returns the renderend html template
+    '''
+    app.run()
