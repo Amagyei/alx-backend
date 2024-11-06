9f310a0aaf0ea4f5d3d124484d909c7f303c4fa8
amagyei <kwame.na@hotmail.com>
2024-11-06 00:41:51 +0000

    task pycodestyle for task 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
-''' Basic flask app that creates a single / route to an index.html page that returns hello world
+''' Basic flask app that creates a single / route
+to an index.html page that returns hello world
 '''
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    ''' function that returns the renderend html template 
+    ''' function that returns the renderend html template
     '''
     return render_template('0-index.html')
 
+
 if __name__ == '__main__':
-    ''' function that returns the renderend html template 
+    ''' function that returns the renderend html template
     '''
     app.run()
-
